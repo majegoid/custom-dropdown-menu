ed791a13e5424cfa7ee14687371c73b20fcb0fe5
majegoid <majegoid@gmail.com>
2023-01-13 19:32:30 -0600

    adds webpack setup

--- src/index.js
+++ src/index.js
@@ -4,20 +4,32 @@ export const createDropdown = (
   valuesArr = ['Choice 1', 'Choice 2', 'Choice 3'],
   isOpenOnHover = false
 ) => {
+  // <select name="" id="">
+  //   <option value="">Option 1</option>
+  //   <option value="">Option 2</option>
+  //   <option value="">Option 3</option>
+  // </select>
+
   const dropdown = document.createElement('select');
-  for (value of valuesArr) {
+
+  for (const value of valuesArr) {
     const dropdownOptionElem = document.createElement('option');
     dropdownOptionElem.value = value;
+    dropdownOptionElem.textContent = value;
     dropdown.appendChild(dropdownOptionElem);
   }
+
   if (dropdownId) {
     dropdown.id = dropdownId;
   }
+
   if (dropdownName) {
     dropdown.name = dropdownName;
   }
+
   if (isOpenOnHover) {
     dropdown.addEventListener('hover');
   }
+
   return dropdown;
 };
